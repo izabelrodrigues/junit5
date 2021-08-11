bcb7559f8b019d4a1d055b02bb6bc79dd335d1c1
Izabel Rodrigues <izabel.castro.rodrigues@gmail.com>
2021-08-10 21:06:52 -0300

    Criação da classe de modelo do Funcionário

---
+++ src/main/java/br/com/izabelrodrigues/tdd/modelo/Funcionario.java
@@ -0,0 +1,36 @@
+/**
+ *
+ */
+package br.com.izabelrodrigues.tdd.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+/**
+ * @author Izabel Rodrigues
+ *
+ */
+public class Funcionario {
+
+	private String nome;
+	private LocalDate dataAdmissao;
+	private BigDecimal salario;
+
+	public Funcionario(String nome, LocalDate dataAdmissao, BigDecimal salario) {
+		this.nome = nome;
+		this.dataAdmissao = dataAdmissao;
+		this.salario = salario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public LocalDate getDataAdmissao() {
+		return dataAdmissao;
+	}
+
+	public BigDecimal getSalario() {
+		return salario;
+	}
+}
