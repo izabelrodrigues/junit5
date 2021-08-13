391bdb5df921962447d79316f76cef473f4d6ad4
Izabel Rodrigues <izabel.castro.rodrigues@gmail.com>
2021-08-12 22:30:43 -0300

    Refatoração na classe de teste: uso de beforeEach

--- src/test/java/br/com/izabelrodrigues/tdd/service/ReajusteServiceTest.java
+++ src/test/java/br/com/izabelrodrigues/tdd/service/ReajusteServiceTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.izabelrodrigues.tdd.modelo.Desempenho;
@@ -19,24 +20,28 @@ import br.com.izabelrodrigues.tdd.modelo.Funcionario;
  */
 public class ReajusteServiceTest {
 
+	private Funcionario funcionario;
+	private ReajusteService reajusteService;
+
+	@BeforeEach
+	public void init() {
+		funcionario = new Funcionario("FUNC_SALARIO_IGUAL_DEZ_MIL", LocalDate.now(), new BigDecimal(10000.00));
+		reajusteService = new ReajusteService();
+	}
+
 	@Test
 	public void reajusteDeveriaSerDeTresPorcentoQuandoFuncionarioNaoBateuAMeta() {
 
-		Funcionario funcionario = new Funcionario("FUNC_SALARIO_IGUAL_DEZ_MIL", LocalDate.now(),
-				new BigDecimal(10000.00));
-		ReajusteService reajusteService = new ReajusteService();
 		reajusteService.efetuarReajuste(funcionario, Desempenho.A_DESEJAR);
 
 		assertEquals(new BigDecimal("10300.00"), funcionario.getSalario(),
 				"Salario do funcionário com desempenho a desejar deveria ter aumento de 3%");
 	}
 
+
 	@Test
 	public void reajusteDeveriaSerDeQuinzePorcentoQuandoFuncionarioTeveDesempenhoBom() {
 
-		Funcionario funcionario = new Funcionario("FUNC_SALARIO_IGUAL_DEZ_MIL", LocalDate.now(),
-				new BigDecimal(10000.00));
-		ReajusteService reajusteService = new ReajusteService();
 		reajusteService.efetuarReajuste(funcionario, Desempenho.BOM);
 
 		assertEquals(new BigDecimal("11500.00"), funcionario.getSalario(),
@@ -46,9 +51,6 @@ public class ReajusteServiceTest {
 	@Test
 	public void reajusteDeveriaSerDeVintePorcentoQuandoFuncionarioTeveDesempenhoBom() {
 
-		Funcionario funcionario = new Funcionario("FUNC_SALARIO_IGUAL_DEZ_MIL", LocalDate.now(),
-				new BigDecimal(10000.00));
-		ReajusteService reajusteService = new ReajusteService();
 		reajusteService.efetuarReajuste(funcionario, Desempenho.OTIMO);
 
 		assertEquals(new BigDecimal("12000.00"), funcionario.getSalario(),
